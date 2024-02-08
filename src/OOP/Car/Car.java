f74f034c5dfedd6133aee3756d82e5e8247cd534
Ziyodaziz Solijonov <solijonovziyodaziz@gmail.com>
2024-02-08 18:36:36 +0500

    Car fayli qo'shildi

--- src/OOP/Car/Car.java
+++ src/OOP/Car/Car.java
@@ -2,28 +2,52 @@ package OOP;
 
 public class Car {
 
-     String name;
-     String model;
-     String color;
-     String price;
-     String avto;
+     private String name;
+     private String model;
+     private String color;
+     private String price;
+     private String avto;
 
-    public Car(String name, String model, String color, String price, String avto){
-        this.name = name;
-        this.model = model;
-        this.color = color;
-        this.price = price;
-        this.avto = avto;
 
-        System.out.println("Nomi " + name);
-        System.out.println("Modeli " + model);
-        System.out.println("Rangi " + color);
-        System.out.println("Narxi " + price);
-        System.out.println("Xususiyati " + avto);
-
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvto() {
+        return avto;
+    }
+
+    public void setAvto(String avto) {
+        this.avto = avto;
+    }
 
 
 }
--- src/OOP/Car/Product.java
+++ src/OOP/Car/Product.java
@@ -5,10 +5,32 @@ import OOP.Car;
 public class Product {
 
     public static void main(String[] args) {
-        Car nexia = new Car("Nexia", "Chevrolet Nexia 3", "Oq", "17 000$", "Avtomat");
-        Car lacetti = new Car("Laceti", "Chevrolet Lacetti Gentra", "Qora", "20 000$", "Mexanika");
-        Car malibu = new Car("Malibu", "Chevrolet Malibu 2 Turbo", "Qora", "30 000$", "Avto");
 
+        Car nexia = new Car();
+        nexia.setName("Nexia");
+        nexia.setModel("Chevrolet Nexia 3");
+        nexia.setColor("Oq");
+        nexia.setPrice("17 000$");
+        nexia.setAvto("Avtomat");
+
+        System.out.println("Nomi " + nexia.getName());
+        System.out.println("Modeli " + nexia.getModel());
+        System.out.println("Rangi " + nexia.getColor());
+        System.out.println("Narxi " + nexia.getPrice());
+        System.out.println("Xususiyati " + nexia.getAvto());
+
+        Car lacceti = new Car();
+        lacceti.setName("Gentra");
+        lacceti.setModel("Chevrolet Laccetti Gentra");
+        lacceti.setColor("Qora");
+        lacceti.setPrice("20 000$");
+        lacceti.setAvto("Avtomat");
+
+        System.out.println("Nomi " + lacceti.getName());
+        System.out.println("Modeli " + lacceti.getModel());
+        System.out.println("Rangi " + lacceti.getColor());
+        System.out.println("Narxi " + lacceti.getPrice());
+        System.out.println("Xususiyati " + lacceti.getAvto());
 
     }
 }
